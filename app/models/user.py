5ba84b6807b69614444539a3d2b1403d5c023a96
Dinesh Yadav <dinesh.yadav@saasbanana.com>
2026-01-12 07:56:35 +0530

    increased string length

--- app/models/user.py
+++ app/models/user.py
@@ -13,8 +13,8 @@ class User(db.Model):
 
 
     def set_password(self,password):
-        self.password_hash=generate_password_hash(password)
+        self.password_hash=generate_password_hash(password)            #it generate hash password and return to services
 
     def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
+        return check_password_hash(self.password_hash,password)       #it check the password in db &give result to login service
 
